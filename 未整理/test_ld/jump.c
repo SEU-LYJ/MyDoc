af996a9d27be7c319f7954a7baf269ccbac56010
liuyingjie <704232442@qq.com>
2022-07-05 14:12:53 +0800

    缓存

---
+++ 未整理/test_ld/jump.c
@@ -0,0 +1,5 @@
+extern void jump_long_away(void);
+void test_jump(void)
+{
+	jump_long_away();	
+}
---
+++ 未整理/test_ld/main.c
@@ -0,0 +1,12 @@
+extern void test_jump(void);
+void jump_long_away(void)
+{
+    int a = 1;
+    int b = a + 2;
+}
+int main()
+{ 
+	test_jump();
+	return 0;
+}
+
